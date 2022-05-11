17b9c78db089fbc958da1ff0c6ab04c92fd7cb26
DiegoEsperidion <81126024+DiegoEsperidion@users.noreply.github.com>
2022-05-11 15:32:40 -0400

    Update cambiarContra.py

--- Pagina_Chilena/Pagina_Chilena/cambiarContra.py
+++ Pagina_Chilena/Pagina_Chilena/cambiarContra.py
@@ -4,18 +4,17 @@ import time
 email = "plynchp_v454k@chyju.com"
 contraseña_Actual = "4gatos#!"
 contraseña_Nueva = "Prismatic015254"
-url = "https://www.weplay.cl/customer/account/edit/changepass/1/"
+url = "https://www.centralgamer.cl/customer/edit"
 driver= webdriver.Chrome("/home/diertech/Downloads/chromedriver")
 
 driver.get(url)
 #Entrar a la pagina 
-driver.find_element_by_id("email").send_keys(email)
-driver.find_element_by_id("pass").send_keys(contraseña_Actual)
-#TIempo de espera para resolver el captCHa
-time.sleep(60)
-
-driver.find_element_by_id("current-password").send_keys(contraseña_Actual)
-driver.find_element_by_id("password").send_keys(contraseña_Nueva)
-driver.find_element_by_id("password-confirmation").send_keys(contraseña_Nueva)
-time.sleep(10)
-driver.find_element_by_xpath('//*[@title="Guardar"]').click()
+driver.find_element_by_name("customer[email]").send_keys(email)
+driver.find_element_by_name("customer[password]").send_keys(contraseña_Actual)
+driver.find_element_by_xpath('//*[@id="submit_login"]').click()
+#Darle a editar
+driver.find_element_by_xpath("/html/body/div/div/div/div[1]/div[1]/div/a").click()
+#Ingreso de nuevos datos y guardar
+driver.find_element_by_name("customer[password]").send_keys(contraseña_Nueva)
+driver.find_element_by_name("customer[password_confirmation]").send_keys(contraseña_Nueva)
+driver.find_element_by_xpath('//*[@id="register_customer"]').click()
